9681ce90f2ece22c4f28f69b125817c2b35dca22
Burati03 <gabrielhenrique@colegiopaulodetarso.com.br>
2025-11-12 14:51:07 +0000

    rota 2

---
+++ flask-exercicios1/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello, Flask !!</h1>', 200
+
+
+@app.route('/versao')
+def versao():
+    versao = "1.1.0"
+    return f"App v{versao}"
+
